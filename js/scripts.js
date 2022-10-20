17f2c055ae1b529bdf5bc6522f7f7a9b09d1bc0e
Michael <89353459+Michael-R-Mac@users.noreply.github.com>
2022-10-20 15:33:08 +0200

    Start js file from scratch
    create a array with several objects for use in the Pokédex app.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
-alert('Hello world');
-let favoriteFood  = 'cake';
-document.write(favoriteFood);
+const pokemonList = [{name: 'Ivysaur', height: 10, types: ['grass', 'poison']}, 
+{name: 'Venusaur', height: 20, types: ['grass', 'poison']}, 
+{name: 'Charizard', height: 17, types: ['fire', 'flying']}];
+console.log(pokemonList);
