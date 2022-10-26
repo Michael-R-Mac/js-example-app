aa10018a37ca50cfc311dd9174c0e4252ef9815b
Michael <89353459+Michael-R-Mac@users.noreply.github.com>
2022-10-26 13:32:41 +0200

    Add js loop, css style and improved html
    - Add simple style to background and text, including a border.
    - Add normalizing to the html, fonts, title and a accessibility tool.
    -Add a JS loop to write in the DOM the Pokémon's and height and spot the bigger ones.
    -Add stylelint to analyze the CSS

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,18 @@
-const pokemonList = [{name: 'Ivysaur', height: 10, types: ['grass', 'poison']}, 
-{name: 'Venusaur', height: 20, types: ['grass', 'poison']}, 
-{name: 'Charizard', height: 17, types: ['fire', 'flying']}];
-console.log(pokemonList);
+// array of objects that contains pokemons and their caracteretics
+const pokemonlist = [{name: 'Ivysaur', height: 1, types: ['grass', 'poison']},
+{name: 'Venusaur', height: 2.0, types: ['grass', 'poison']},
+{name: 'Charizard', height: 1.7, types: ['fire', 'flying']}];
+
+// variable for the lenght of the array to be used in the for loop
+let len = pokemonlist.length
+
+// for loop to write the pokemons and height in the DOM
+for(let i=0; i < len; i++) {
+  document.write('<p>The pokemon ' + pokemonlist[i].name + ' has a height of '
+  + pokemonlist[i].height + ' m.</p>');
+
+  // conditional for bigger pokemons
+  if(pokemonlist[i].height >= 2) {
+    document.write('<p>The ' + pokemonlist[i].name + ' pokemon is big.' + '</p>');
+  }
+}
