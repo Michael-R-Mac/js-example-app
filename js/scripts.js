81cab34b83a8e8167b3f4974d7bb9086d35a0047
Michael <89353459+Michael-R-Mac@users.noreply.github.com>
2022-11-16 01:15:23 +0100

    introduce modal to show pokemon details
    -introduce css for the modal, its components and the isvisible vs display none.
    -introduce in index a div for the modal.
    - change JS from console view of the the details to modal. The details shown are name, height and image.

--- js/scripts.js
+++ js/scripts.js
@@ -20,10 +20,64 @@ let pokemonrepository = (function () {
     return pokemonlist;
   }
 
-  // function that enables the button to show pokemons of the variable in the console
+  // function that enables the button to show pokemons of the variable in a modal
   function showDetails(pokemon) {
-    loadDetails(pokemon).then(function () {
-      console.log(pokemon);
+    loadDetails(pokemon).then(function() {
+
+      let modalContainer = document.querySelector('#modal-container');
+
+      function showModal(title, text, img) {
+        modalContainer.innerHTML = '';
+        let modal = document.createElement('div');
+        modal.classList.add('modal');
+    
+        let closeButtonElement = document.createElement('button');
+        closeButtonElement.classList.add('modal-close');
+        closeButtonElement.innerText = 'Close';
+        closeButtonElement.addEventListener('click', hideModal);
+    
+        let titleElement = document.createElement('h1');
+        titleElement.innerText = title;
+    
+        let contentElement = document.createElement('p');
+        contentElement.innerText = text;
+
+        let myImage = document.createElement('img');
+        myImage.src = img;
+    
+        modal.appendChild(closeButtonElement);
+        modal.appendChild(titleElement);
+        modal.appendChild(contentElement);
+        modal.appendChild(myImage);
+        modalContainer.appendChild(modal);
+        
+        
+        modalContainer.classList.add('is-visible');
+      }
+    
+      function hideModal() {
+        modalContainer.classList.remove('is-visible');
+      }
+    
+      window.addEventListener('keydown', (e) => {
+        if (e.key === 'Escape' && modalContainer.classList.contains('is-visible')) {
+          hideModal();  
+        }
+      });
+      
+      modalContainer.addEventListener('click', (e) => {
+        // Since this is also triggered when clicking INSIDE the modal
+        // We only want to close if the user clicks directly on the overlay
+        let target = e.target;
+        if (target === modalContainer) {
+          hideModal();
+        }
+      });
+    
+      document.querySelector('.page-pokemon-button').addEventListener('click', () => {
+        showModal(pokemon.name, 'Height: ' + pokemon.height, pokemon.imageUrl);
+      });
+      
     });
   }
 
